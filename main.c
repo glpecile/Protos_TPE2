345bfd9f87cad48d4bf11a8c7ef8ea85c3a80fac
Julian Arce <juarce@itba.edu.ar>
2021-11-21 17:41:23 -0300

    Using pop3 parameters convention.
    Co-authored-by: Roberto Catalan <rcatalan@itba.edu.ar>

--- main.c
+++ main.c
@@ -35,8 +35,6 @@
 
 static bool done = false;
 
-static struct socks5args *sock_args;
-
 static void
 sigterm_handler(const int signal) {
     printf("Signal %d, cleaning up and exiting\n", signal);
@@ -334,7 +332,7 @@ static int initialize_server(int port) {
         goto finally;
     }
 
-    selector = selector_new(1024);
+    selector = selector_new(INITIAL_ELEMENTS);
     if (selector == NULL) {
         err_msg = "unable to create selector";
         goto finally;
@@ -399,18 +397,13 @@ static int initialize_server(int port) {
         printf("about to close the server_ipv4\n");
         close(server_ipv4);
     }
-    free(sock_args);
+    free(parameters);
     printf("closing main safely...\n");
     return ret;
 }
 
 int
 main(const int argc, char **argv) {
-    sock_args = malloc(sizeof(struct socks5args));
-    if (sock_args == NULL) {
-        perror("Unable to allocate memory for arguments.");
-        exit(-1);
-    }
     initialize_pop3_parameters_options();
     parse_parameters(argc, argv);
 
@@ -420,175 +413,8 @@ main(const int argc, char **argv) {
 
     if (setvbuf(stdout, NULL, _IONBF, 0)) {
         perror("Unable to disable buffering");
-        free(sock_args);
+        free(parameters);
         exit(-1);
     }
-    return initialize_server(sock_args->socks_port);
-
-//    const char *err_msg = NULL;
-//    selector_status ss = SELECTOR_SUCCESS;
-//    fd_selector selector = NULL;
-//
-//    int server_ipv6 = -1;
-//    int server_ipv4 = -1;
-//
-//
-//
-//    /**
-//     * Creamos el socket para IPv6
-//     */
-//    struct sockaddr_in6 addr6;
-//    memset(&addr6, 0, sizeof(addr6));
-//    addr6.sin6_family = AF_INET6;
-//    addr6.sin6_addr = in6addr_any;
-//    addr6.sin6_port = htons(sock_args->socks_port);
-//
-//    server_ipv6 = socket(addr6.sin6_family, SOCK_STREAM, IPPROTO_TCP);
-//    if (server_ipv6 < 0) {
-//        err_msg = "Unable to create socket ipv6";
-//        goto finally;
-//    }
-//
-//    fprintf(stdout, "Listening on TCP port %d\n", sock_args->socks_port);
-//
-//    // man 7 ip. no importa reportar nada si falla.
-//    setsockopt(server_ipv6, SOL_SOCKET, SO_REUSEADDR, &(int) {1}, sizeof(int));
-//    setsockopt(server_ipv6, SOL_IPV6, IPV6_V6ONLY, &(int) {1}, sizeof(int));
-//
-//    if (bind(server_ipv6, (struct sockaddr *) &addr6, sizeof(addr6)) < 0) {
-//        err_msg = "unable to bind socket ipv6";
-//        goto finally;
-//    }
-//
-//    if (listen(server_ipv6, PENDING_CONNECTIONS) < 0) {
-//        err_msg = "unable to listen ipv6";
-//        goto finally;
-//    }
-//
-//    /**
-//     * Creamos el socket para IPv4
-//     */
-//    struct sockaddr_in addr;
-//    memset(&addr, 0, sizeof(addr));
-//    addr.sin_family = AF_INET;
-//    addr.sin_addr.s_addr = htonl(INADDR_ANY);
-//    addr.sin_port = htons(sock_args->socks_port);
-//
-//    server_ipv4 = socket(addr.sin_family, SOCK_STREAM, IPPROTO_TCP);
-//    if (server_ipv4 < 0) {
-//        err_msg = "Unable to create socket ipv4";
-//        goto finally;
-//    }
-//
-//    fprintf(stdout, "Listening on TCP port %d\n", sock_args->socks_port);
-//
-//    // man 7 ip. no importa reportar nada si falla.
-//    setsockopt(server_ipv4, SOL_SOCKET, SO_REUSEADDR, &(int) {1}, sizeof(int));
-//
-//    if (bind(server_ipv4, (struct sockaddr *) &addr, sizeof(addr)) < 0) {
-//        err_msg = "unable to bind socket ipv4";
-//        goto finally;
-//    }
-//
-//    if (listen(server_ipv4, PENDING_CONNECTIONS) < 0) {
-//        err_msg = "unable to listen ipv4";
-//        goto finally;
-//    }
-//
-//    /**
-//     * Registrar sigterm es útil para terminar el programa normalmente.
-//     * Esto ayuda mucho en herramientas como valgrind.
-//     */
-//    signal(SIGTERM, sigterm_handler);
-//    signal(SIGINT, sigterm_handler);
-//
-//    if (selector_fd_set_nio(server_ipv6) == -1) {
-//        err_msg = "getting server socket flags";
-//        goto finally;
-//    }
-//    if (selector_fd_set_nio(server_ipv4) == -1) {
-//        err_msg = "getting server socket flags";
-//        goto finally;
-//    }
-//    //1-Iniciar la libreria
-//    const struct selector_init conf = {
-//            .signal = SIGALRM,
-//            .select_timeout = {
-//                    .tv_sec  = 10,
-//                    .tv_nsec = 0,
-//            },
-//    };
-//    if (0 != selector_init(&conf)) {
-//        err_msg = "initializing selector";
-//        goto finally;
-//    }
-//
-//    selector = selector_new(1024);
-//    if (selector == NULL) {
-//        err_msg = "unable to create selector";
-//        goto finally;
-//    }
-//    const struct fd_handler socks_handler = {
-//            .handle_read       = socks_passive_accept,
-//            .handle_write      = NULL,
-//            .handle_close      = NULL, // nada que liberar
-//    };
-//    ss = selector_register(selector, server_ipv6, &socks_handler, OP_READ, NULL);
-//    if (ss != SELECTOR_SUCCESS) {
-//        err_msg = "registering fd";
-//        goto finally;
-//    }
-//    ss = selector_register(selector, server_ipv4, &socks_handler, OP_READ, NULL);
-//    if (ss != SELECTOR_SUCCESS) {
-//        err_msg = "registering fd";
-//        goto finally;
-//    }
-//    for (; !done;) {
-//        fprintf(stdout,"Waiting for incoming connection...\n");
-//        err_msg = NULL;
-//        ss = selector_select(selector);
-//        if (ss != SELECTOR_SUCCESS) {
-//            err_msg = "serving";
-//            goto finally;
-//        }
-//    }
-//    if (err_msg == NULL) {
-//        err_msg = "closing";
-//    }
-//
-//    int ret = 0;
-//
-//    /**
-//     * Finally
-//     */
-//    finally:
-//    if (ss != SELECTOR_SUCCESS) {
-//        fprintf(stderr, "%s: %s\n", (err_msg == NULL) ? "" : err_msg,
-//                ss == SELECTOR_IO
-//                ? strerror(errno)
-//                : selector_error(ss));
-//        ret = 2;
-//    } else if (err_msg) {
-//        perror(err_msg);
-//        ret = 1;
-//    }
-//    if (selector != NULL) {
-//        selector_destroy(selector);
-//    }
-//    printf("about to close the selector\n");
-//    selector_close();
-//    printf("about to destroy the pool\n");
-//    socks_pool_destroy();
-//
-//    if (server_ipv6 >= 0) {
-//        printf("about to close the server_ipv6\n");
-//        close(server_ipv6);
-//    }
-//    if (server_ipv4 >= 0) {
-//        printf("about to close the server_ipv4\n");
-//        close(server_ipv4);
-//    }
-//    free(sock_args);
-//    printf("closing main safely...\n");
-//    return ret;
+    return initialize_server(parameters->port);
 }
--- makefile
+++ makefile
@@ -1,6 +1,6 @@
 .PHONY : all clean test cleanTest server
 COMPILER=gcc
-CFLAGS = --std=c11 -fsanitize=address -pedantic -pedantic-errors -Wall -Wextra -Werror -Wno-unused-parameter -Wno-implicit-fallthrough -D_POSIX_C_SOURCE=200112L -Wunused-but-set-variable -g
+CFLAGS = --std=c11 -fsanitize=address -pedantic -pedantic-errors -Wall -Wextra -Werror -Wno-unused-parameter -Wno-implicit-fallthrough -D_POSIX_C_SOURCE=200112L -g
 LIBS = -l pthread
 all: main
 clean:
