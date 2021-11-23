715d0deb31a59618572b8c60181d5b6d750897cd
Paula Domingues <pdomingues@itba.edu.ar>
2021-11-22 21:58:00 -0300

    Admin Parser working. Options are missing
    Co-authored-by: Gian Luca Pecile <gpecile@itba.edu.ar>

--- include/admin.h
+++ include/admin.h
@@ -8,6 +8,18 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 
+enum command_enum {
+    START, GET_STATS, GET_CURRENT_CON, SET_AUTH, SET_MEM_SPACE, SET_TIMEOUT, HELP
+};
+
+struct current_command {
+    enum command_enum command;
+};
+
+typedef struct current_command *current_command;
+
+extern current_command current;
+
 void udp_read(struct selector_key *key);
 
 void udp_write(struct selector_key *key);
--- src/admin.c
+++ src/admin.c
@@ -1,12 +1,51 @@
 #include "../include/admin.h"
 
-#define BUFF_SIZE 100
+#define BUFF_SIZE 400
 
-const char *execute(char *string) {
-    return "bitte\n";
+current_command current;
+
+char *get_response() {
+    if (current->command == START) {
+        return "start\n";
+    } else if (current->command == GET_STATS) {
+        return "get stats\n";
+    } else if (current->command == GET_CURRENT_CON) {
+        return "get current con\n";
+    } else if (current->command == SET_AUTH) {
+        return "set auth\n";
+    } else if (current->command == SET_MEM_SPACE) {
+        return "set mem space\n";
+    } else if (current->command == SET_TIMEOUT) {
+        return "set timeout\n";
+    } else if (current->command == HELP) {
+        return "help\n";
+    }
+    return "Invalid command. Try typing 'HELP' to get more information\n"
+           "Keep in mind commands are case sensitive\n";
+
+}
+
+
+void parse_udp(char *buffer, int length) {
+    if (strcmp(buffer, "START\n") == 0) {
+        current->command = START;
+    } else if (strcmp(buffer, "GET STATS\n") == 0) {
+        current->command = GET_STATS;
+    } else if (strcmp(buffer, "GET CURRENT_CON\n") == 0) {
+        current->command = GET_CURRENT_CON;
+    } else if (strcmp(buffer, "SET AUTH\n") == 0) {
+        current->command = SET_AUTH;
+    } else if (strcmp(buffer, "SET MEM_SPACE\n") == 0) {
+        current->command = SET_MEM_SPACE;
+    } else if (strcmp(buffer, "SET TIMEOUT\n") == 0) {
+        current->command = SET_TIMEOUT;
+    } else if (strcmp(buffer, "HELP\n") == 0) {
+        current->command = HELP;
+    }
 }
 
 void udp_read(struct selector_key *key) {
+    current = malloc(sizeof(*current));
     struct sockaddr_storage clntAddr;            // Client address
     socklen_t clntAddrLen = sizeof(clntAddr);
     char buffer[BUFF_SIZE] = {0};
@@ -16,6 +55,7 @@ void udp_read(struct selector_key *key) {
         fprintf(stderr, "recvfrom() failed: %s\n", strerror(errno));
         exit(1);
     } else {
+        parse_udp(buffer, (int) valread);
         printf("Parsear y hacer algo\n");
         selector_set_interest_key(key, OP_WRITE);
     }
@@ -24,11 +64,9 @@ void udp_read(struct selector_key *key) {
 void udp_write(struct selector_key *key) {
     struct sockaddr_storage clntAddr;            // Client address
     socklen_t clntAddrLen = sizeof(clntAddr);
-
-    char buffer[BUFF_SIZE] = {0};
-    const char *answer;
-    answer = execute(buffer);
-    int bytes_to_send = strlen(answer);
+    char answer[BUFF_SIZE];
+    strcpy(answer,get_response());
+    int bytes_to_send = (int) strlen(answer);
     ssize_t numBytesSent = sendto(key->fd, answer, bytes_to_send, 0, (struct sockaddr *) &clntAddr, clntAddrLen);
     //reset parser
     if (numBytesSent < 0) {
@@ -38,6 +76,6 @@ void udp_write(struct selector_key *key) {
         fprintf(stderr, "sendto() sent unexpected number of bytes.\n");
         exit(1);
     }
-//    }
     selector_set_interest_key(key, OP_READ);
+    free(current);
 }
